487eb497fcd76573e3238f626cc926fda2f4084f
Pao Zambrano <pao.zambranoo_@hotmail.com>
2026-01-14 16:30:56 -0500

    add .gitignore file

---
+++ scripts/db_handler.py
@@ -0,0 +1,24 @@
+import mysql.connector
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+class DBHandler:
+    def __init__(self):
+        self.connection = mysql.connector.connect(
+            host=os.getenv("DB_HOST"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            database=os.getenv("DB_NAME"),
+            port=os.getenv("DB_PORT")
+        )
+        self.cursor = self.connection.cursor(dictionary=True)
+
+    def get_book_by_title(self, title):
+        query = "SELECT * FROM books WHERE title = %s"
+        self.cursor.execute(query, (title,))
+        return self.cursor.fetchone()
+    
+    def close(self):
+        self.cursor.close()
+        self.connection.close()
