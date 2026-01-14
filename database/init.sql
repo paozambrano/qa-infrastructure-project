601ba301cd42ca95839ee182daa759737da34756
Pao Zambrano <pao.zambranoo_@hotmail.com>
2026-01-14 15:16:36 -0500

    create init.sql to initialize bookstore table and entries

---
+++ database/init.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS books (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    author VARCHAR(255) NOT NULL,
+    created_at TIMESTAMPS DEFAULT CURRENT_TIMESTAMP
+);
+INSERT INTO books(title, author) VALUES ('The Lean Startup', 'Eric Ries')
