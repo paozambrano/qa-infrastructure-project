9fcd6e45bd2a9ccfc9fb1681cf22bc835d5f752c
Pao Zambrano <pao.zambranoo_@hotmail.com>
2026-01-14 15:20:13 -0500

    typo fixed

--- database/init.sql
+++ database/init.sql
@@ -2,6 +2,6 @@ CREATE TABLE IF NOT EXISTS books (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     author VARCHAR(255) NOT NULL,
-    created_at TIMESTAMPS DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 INSERT INTO books(title, author) VALUES ('The Lean Startup', 'Eric Ries')
